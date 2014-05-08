9bca42575b78f4bec4e87e5b63ae569a1ac74c20
Erique Bomfim <erique.bomfim@prodeb.ba.gov.br>
2014-05-08 13:58:01 -0300

    Adding mouse cursor options

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener(function(tab){
 	chrome.tabs.executeScript(null,{
 		file:'grab.js'
+	});
+	chrome.tabs.insertCSS(null,{
+		file:'style.css'
 	})
 });
--- grab.js
+++ grab.js
@@ -12,16 +12,37 @@
 
 var ctrlPressed,mousePressed,mouseY;
 
+var toogleMouse = function(option){
+
+    var _currentClassName = document.body.className;
+	
+	if (option == 1 && _currentClassName.indexOf("hand_opn") < 0)	
+		document.body.className += " hand_opn";	
+	if (option == 2 && _currentClassName.indexOf("hand_cls") < 0)	
+		document.body.className += " hand_cls";
+	if (option == 3)	
+		document.body.className = _currentClassName.replace(" hand_cls","");
+	if (option == 0)	
+		document.body.className = _currentClassName.replace(" hand_opn","");
+}
+
 document.onkeydown = function(event){
-    if (event.which == 17 ) ctrlPressed = true;
+    if (event.which == 17 ){
+		ctrlPressed = true;
+		toogleMouse(1);
+	}
 };
 document.onkeyup = function(event){
-    if (event.which == 17 ) ctrlPressed = false;
+    if (event.which == 17 ){
+		ctrlPressed = false;
+		toogleMouse(0);
+	}
 };
 document.onmousedown = function(event){
     if( ctrlPressed && event.which == 1){
 		mousePressed = true;
 		event.preventDefault();
+		toogleMouse(2);
 		return false;
 	}
 	return event;
@@ -29,6 +50,7 @@ document.onmousedown = function(event){
 document.onmouseup = function(event){
     if( ctrlPressed && event.which == 1){
 		mousePressed = false;
+		toogleMouse(3);
 	}
 };
 document.onmousemove = function(event){
